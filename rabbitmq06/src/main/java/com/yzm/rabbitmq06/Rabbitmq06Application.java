749b1e1348c6928f032c79ae848dd2211e8a74ca
yezhimincxvxb <395995486@qq.com>
2021-11-25 10:17:00 +0800

    mq

---
+++ rabbitmq06/src/main/java/com/yzm/rabbitmq06/Rabbitmq06Application.java
@@ -0,0 +1,13 @@
+package com.yzm.rabbitmq06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rabbitmq06Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Rabbitmq06Application.class, args);
+    }
+
+}
---
+++ rabbitmq06/src/test/java/com/yzm/rabbitmq06/Rabbitmq06ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yzm.rabbitmq06;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rabbitmq06ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
